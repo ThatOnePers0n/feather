710595223d251c84663368080e8ad5e0680a78ed
AndrewTheCoolAlias <andrewthecoolalias@gmail.com>
2023-01-04 17:34:53 -0800

    Update splash.js

--- main/splash.js
+++ main/splash.js
@@ -1,28 +1,12 @@
-// script to generate random greetings
+var text_location = document.getElementById("subtitle");
+var splash = new Array ();
+splash[0] = "Hello!";
+splash[1] = "Welcome!";
+splash[2] = "this is a test";
+splash[3] = "These games are not from 3kh0.github.io";
+splash[4] = "Brought to you by arc!";
+splash[5] = "Made with HTML";
+splash[6] = "Made with Javascript";
+var i = Math.floor(7*Math.random())
 
-function changeSub(num) {
-  document.getElementById("splash").innerText = say[num];
-}
-
-var say = [
-  "Welcome.",
-  "Hello!",
-  "Does this even work?",
-  "Welcome to my rad website",
-
-
-];
-console.log("[INFO] " + say.length + " splash texts were loaded!")
-// pick a random greeting
-var howmany = say.length;
-var bRand = 0;
-bRand = Math.random();
-bRand = Math.floor(bRand * howmany);
-sayWhat = say[bRand];
-document.getElementById("subtitle").innerText = sayWhat;
-
-function changeSplash(num) {
-  document.getElementById("subtitle").innerText = say[num];
-  var ret = "Set current splash to splash " + num + ", " + say[num];
-  return ret;
-}
+text_location.innerHTML(splash[i]);
