21ba4c4fbb07428aaaeb32c2ac5f00d741ad1e83
ThatOnePerson <87896375+RetroWonder@users.noreply.github.com>
2023-01-04 20:53:22 -0700

    Update splash.js

--- main/splash.js
+++ main/splash.js
@@ -5,5 +5,5 @@ splash[2] = "Almost carbon neutral!";
 splash[3] = "Fast loading times!";
 splash[4] = "Protected by Unbed! (www.unbed.gq)";
 splash[5] = "Please don't iFrame me!";
-var i = Math.floor(5*Math.random());
+var i = Math.floor(6*Math.random());
 text_location.innerHTML = splash[i];
