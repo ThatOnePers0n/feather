2965694a5a565e40ccfbbb2c74683ff7bfa8009c
AndrewTheCoolAlias <andrewthecoolalias@gmail.com>
2023-01-04 18:41:08 -0800

    Update splash.js

--- main/splash.js
+++ main/splash.js
@@ -1,6 +1,7 @@
 var text_location = document.getElementById("subtitle");
 var splash = new Array ();splash[0] = "Hello!";
-splash[1] = "Welcome!";splash[2] = "this is a test";
+splash[1] = "Welcome!";
+splash[2] = "this is a test";
 splash[3] = "These games are not from 3kh0.github.io";
 splash[4] = "Brought to you by arc!";splash[5] = "Made with HTML";
 splash[6] = "Made with Javascript";
