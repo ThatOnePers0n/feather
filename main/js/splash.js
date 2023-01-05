92db04ee84213c860073b5561f40be2cd1b01eb1
ThatOnePerson <87896375+RetroWonder@users.noreply.github.com>
2023-01-04 20:21:27 -0700

    File location update

---
+++ main/js/splash.js
@@ -0,0 +1,9 @@
+var text_location = document.getElementById("subtitle");
+var splash = new Array ();splash[0] = "Always Lightweight & Ad Free.";
+splash[1] = "Light as a feather, with almost no trackers.";
+splash[2] = "Almost carbon neutral!";
+splash[3] = "Fast loading times!";
+splash[4] = "Protected by Unbed! (www.unbed.gq)";
+splash[5] = "Please don't iFrame me!";
+var i = Math.floor(7*Math.random());
+text_location.innerHTML = splash[i];
