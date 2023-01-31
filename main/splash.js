b89b57002aa97e4ba9e73782fd3d421f24323cb9
ThatOnePerson <87896375+RetroWonder@users.noreply.github.com>
2023-01-31 16:36:20 -0700

    Update splash.js

---
+++ main/splash.js
@@ -0,0 +1,7 @@
+var text_location = document.getElementById("subtitle");
+var splash = new Array ();splash[0] = "Always Lightweight & Ad Free.";
+splash[1] = "Light as a feather, with almost no trackers.";
+splash[2] = "Almost carbon neutral!";
+splash[3] = "Fast loading times!";
+var i = Math.floor(4*Math.random());
+text_location.innerHTML = splash[i];
